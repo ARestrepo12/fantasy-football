24dc6527ebc1543fb2fef2ee00b71b5b0f2ecbc0
unknown <arestre@ncsu.edu>
2022-11-24 20:52:13 -0500

    delete import

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -6,5 +6,5 @@ import { idbPromise } from "../../utils/helpers";
 import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { QUERY_CHECKOUT } from "../../utils/queries";
-import { useLazyQuery } from "@apollo/client";
+
 
