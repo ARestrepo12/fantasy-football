2ec0ee7ed84f9db2e9332466cdbd3487e66d4097
unknown <arestre@ncsu.edu>
2022-11-28 16:03:16 -0500

    index change

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -5,6 +5,3 @@ import Auth from "../../utils/auth";
 import { idbPromise } from "../../utils/helpers";
 import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
-import { QUERY_CHECKOUT } from "../../utils/queries";
-
-
