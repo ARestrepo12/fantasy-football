56f96785ecdcf50a0ee88d93edf505d1aa034e8a
unknown <arestre@ncsu.edu>
2022-11-29 19:54:11 -0500

    clean up code

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -4,4 +4,3 @@ import Logo from "../../assets/lg-logo.png";
 import Auth from "../../utils/auth";
 import { idbPromise } from "../../utils/helpers";
 import { useStoreContext } from "../../utils/GlobalState";
-import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
