e4f332632f4da6b936c40e3e6a3037f7b09d3a3e
unknown <arestre@ncsu.edu>
2022-12-06 21:31:41 -0500

    updates

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,4 +2,3 @@ import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/lg-logo.png";
 import Auth from "../../utils/auth";
-import { idbPromise } from "../../utils/helpers";
